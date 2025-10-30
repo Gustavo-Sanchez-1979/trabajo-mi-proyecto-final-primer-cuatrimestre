6cd2bf1209816a879974d46682eba0791b6be136
Gustavo Sánchez-1979 <gs0246772@gmail.com>
2025-10-30 19:52:06 -0300

    se agrega boton volver por si ingresan a administrador de empleados y no estan registrados puedan volver a pagina principal

--- trabajo_practico_dos_tablas/login.php
+++ trabajo_practico_dos_tablas/login.php
@@ -57,28 +57,32 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-<meta charset="UTF-8">
-<title>Ingreso al sistema</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <title>Ingreso al sistema</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body class="bg-light">
-<div class="container mt-5 col-md-4">
-  <div class="card shadow p-4">
-    <h3 class="text-center mb-3">Iniciar sesión</h3>
-    <?php if(isset($error)) echo "<div class='alert alert-danger'>$error</div>"; ?>
-    <form method="POST">
-      <div class="mb-3">
-        <label>Usuario</label>
-        <input type="text" name="usuario" class="form-control" required>
-      </div>
-      <div class="mb-3">
-        <label>Contraseña</label>
-        <input type="password" name="contrasenia" class="form-control" required>
-      </div>
-      <button class="btn btn-primary w-100" type="submit">Ingresar</button>
-    </form>
+  <div class="container mt-5 col-md-4">
+    <div class="card shadow p-4">
+      <h3 class="text-center mb-3">Iniciar sesión</h3>
+      <?php if (isset($error)) echo "<div class='alert alert-danger'>$error</div>"; ?>
+      <form method="POST">
+        <div class="mb-3">
+          <label>Usuario</label>
+          <input type="text" name="usuario" class="form-control" required>
+        </div>
+        <div class="mb-3">
+          <label>Contraseña</label>
+          <input type="password" name="contrasenia" class="form-control" required>
+        </div>
+        <button class="btn btn-primary w-100" type="submit">Ingresar</button>
+        <a href="/trabajo-mi-proyecto-final-primer-cuatrimestre/index.php" class="btn btn-outline-secondary w-100 mt-3">Volver</a>
+      </form>
+    </div>
   </div>
-</div>
 </body>
+
 </html>
